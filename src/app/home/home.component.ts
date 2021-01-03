51cea26c8dee2f8b1f0caedc7e37216f8bec79b7
Shelby <tateremail@gmail.com>
2021-01-03 17:04:58 -0600

    built observables

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,19 +1,47 @@
-import { Component, OnInit } from '@angular/core';
-import { interval } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+
+  private firstObsSubscription: Subscription; 
 
   constructor() { }
 
   ngOnInit() {
-    interval(1000).subscribe(count => {
-      console.log(count);
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count);
+    // });
+    const customIntervalObs = Observable.create(observer => {
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if (count == 2) {
+          observer.complete();
+        }; 
+        if (count > 3) {
+          observer.error(new Error('Count is greater than 3'));
+        };
+        count++;
+      }, 1000);
     });
+    
+  this.firstObsSubscription = customIntervalObs.subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.log(error);
+      alert(error.message);
+    }, () => {
+      console.log('Completed!');
+    });
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
